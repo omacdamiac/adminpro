fc40c321fb7f5dac3adf04d2c35014492f4b7107
Omac Villavicencio <omac@OMAC.local>
2020-08-05 22:48:07 -0500

    fin seccion 3 - interfaces

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.scss']
+})
+export class NopagefoundComponent {
+year = new Date().getFullYear();
+
+}
